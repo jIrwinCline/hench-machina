e68a409bd4fed93b5e06e66f2740fc1751ce6a77
Student <example@example.com>
2019-10-01 09:19:22 -0700

    add two companies to a mock db in body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NewsFeed from './NewsFeed';
 import Navigation from './Navigation';
 import MasterForm from './MasterForm';
 
-export default function Header() {
+let MasterCompanyList = [
+  {
+    companyName: "Corporate Crime Control Association",
+    coreServices: [
+      "Patrol",
+      "Loss Prevention",
+      "Entry Access",
+      "Surveilance",
+      "Event Security",
+      "Personnel Protection",
+      "Estate and Residential Protection",
+    ],
+    specialServices: [
+      "Transient Disbursement Services",
+      "Street Crime Unit",
+      "Security Consulting",
+      "Drone Services (Day and Night FLIR)",
+      "Executive Protection",
+      "Workplace Violence Details",
+      "Mobile Incident Command Center",
+      "Security Training",
+    ],
+    discreet: true,
+    armedServices = true,
+    handsOn: true,
+    hourlyRange: [23, 30],
+    contactNumber: 9999999999,
+    address: "21328 Highway 99E Aurora, OR 97002"
+
+  },
+  {
+    companyName: "Northwest Enforcement",
+    coreServices: [
+      "Patrol",
+      "Loss Prevention",
+      "Entry Access",
+      "Surveilance",
+      "Event Security",
+      "Executive/Personnel Protection",
+      "Estate and Residential Protection",
+      "Secure Transport",
+    ],
+    specialServices: [
+      "Street Crime Unit",
+      "Security Training",
+    ],
+    discreet: true,
+    armedServices = true,
+    handsOn: true,
+    hourlyRange: [23, 30],
+    contactNumber: 9999999999,
+    address: "21328 Highway 99E Aurora, OR 97002"
+
+  }
+]
+
+export default class Body extends Component {
   return (
       <div>
       <NewsFeed />
