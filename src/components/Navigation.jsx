e939f7c28453bcf3276eb95e1ec4543ee36b2d47
jirwincline <jakobinwins@gmail.com>
2019-09-30 20:27:22 -0700

    add Navigation component contents

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import FindButton from './FindButton';
 
 export default function Navigation() {
   return (
       <div>
-      
+        <h2>Security</h2>
+        <hr/>
+        <h3>Find Services Near You</h3>
+        <FindButton/>
+        <button>up</button>
+        <button>down</button>
       </div>
   )
 };
