508850a6ff2d93b6a8f24b51537e6c26da9c4f6c
Student <example@example.com>
2019-10-01 08:34:19 -0700

    add news columns

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -2,8 +2,39 @@ import React from 'react';
 
 export default function NewsFeed() {
   return (
-      <div>
-      
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card">
+            <div className="header">content</div>
+              <div className="img-top"><img src="https://www.securitymagazine.com/ext/resources/Default-images/Responsive-Defaults/guards4-900px.jpg?height=168&t=1472047046&width=275" alt="Security Guy"/></div>
+              <div class="card-title">content</div>
+            <div class="card-body">
+              <div class="card-text">content</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div className="header">content</div>
+              <div className="img-top"><img src="https://www.securitymagazine.com/ext/resources/Default-images/Responsive-Defaults/guards4-900px.jpg?height=168&t=1472047046&width=275" alt="Security Guy"/></div>
+              <div class="card-title">content</div>
+            <div class="card-body">
+              <div class="card-text">content</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div className="header">content</div>
+              <div className="img-top"><img src="https://www.securitymagazine.com/ext/resources/Default-images/Responsive-Defaults/guards4-900px.jpg?height=168&t=1472047046&width=275" alt="Security Guy"/></div>
+              <div class="card-title">content</div>
+            <div class="card-body">
+              <div class="card-text">content</div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
   )
 };
