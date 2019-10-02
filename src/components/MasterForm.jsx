01bc21c01d5baf9f6d086df18ec8c4b05e4d9913
Student <example@example.com>
2019-10-02 13:40:18 -0700

    put in container

--- src/components/MasterForm.jsx
+++ src/components/MasterForm.jsx
@@ -117,52 +117,52 @@ export default class MasterForm extends Component {
             );
         })
         return(
-            <>
+            <div className="container-fluid">
             <h3>Select Services Needed</h3>
             <p>and we'll find companies that can provide</p>
-        
-            {inputs}
-            <h3>Answer a few questions about the service you're wanting</h3>
-            <p>we'll further refine the results</p>
-            <div className="form-group">
-                <label htmlFor="RadioGroup">Do you need a company known for going hands on for arresting?</label>
-                <RadioGroup horizontal>
-                    <RadioButton value="yes">
-                        Yes
-                    </RadioButton>
-                    <RadioButton value="no">
-                        No
-                    </RadioButton>
-                </RadioGroup>
-                <label htmlFor="RadioGroup">What price range are you looking to pay for services? (typically, higher prices are associated with better a quality of service)</label>
-                <RadioGroup horizontal>
-                    <RadioButton value="low">
-                        Low Cost
-                    </RadioButton>
-                    <RadioButton value="med">
-                        Medium Cost
-                    </RadioButton>
-                    <RadioButton value="high">
-                        High Cost (premium services)
-                    </RadioButton>
-                </RadioGroup>
+            <form handleSubmit={this.handleSubmit}>
+                {inputs}
+                <h3>Answer a few questions about the service you're wanting</h3>
+                <p>we'll further refine the results</p>
+                <div className="form-group">
+                    <label htmlFor="RadioGroup">Do you need a company known for going hands on for arresting?</label>
+                    <RadioGroup horizontal>
+                        <RadioButton value="yes">
+                            Yes
+                        </RadioButton>
+                        <RadioButton value="no">
+                            No
+                        </RadioButton>
+                    </RadioGroup>
+                    <label htmlFor="RadioGroup">What price range are you looking to pay for services? (typically, higher prices are associated with better a quality of service)</label>
+                    <RadioGroup horizontal>
+                        <RadioButton value="low">
+                            Low Cost
+                        </RadioButton>
+                        <RadioButton value="med">
+                            Medium Cost
+                        </RadioButton>
+                        <RadioButton value="high">
+                            High Cost (premium services)
+                        </RadioButton>
+                    </RadioGroup>
+                </div>
+                <p>Anything else the service provider should know?</p>
+                <div className="form-group">
+                    <label htmlFor="info">Is there additional information that the service provider should know about?</label>
+                    <input
+                        className="form-control"
+                        id="addInfo"
+                        name="addInfo"
+                        type="text"
+                        placeholder="Add Important Info"
+                        value={this.state.addInfo} 
+                        onChange={this.handleChange}
+                    />
+                    <button className="btn btn-success btn-block">Submit</button>
+                </div> 
+            </form>
             </div>
-            <p>Anything else the service provider should know?</p>
-            <div className="form-group">
-                <label htmlFor="info">Is there additional information that the service provider should know about?</label>
-                <input
-                    className="form-control"
-                    id="addInfo"
-                    name="addInfo"
-                    type="text"
-                    placeholder="Add Important Info"
-                    value={this.state.addInfo} 
-                    onChange={this.handleChange}
-                />
-                <button className="btn btn-success btn-block">Submit</button>
-            </div> 
-            </>
-
         )
     }
 }
