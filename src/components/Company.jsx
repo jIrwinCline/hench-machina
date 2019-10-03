6478e59e07ba23196242d98c7e179aaab8e7bd7b
Student <example@example.com>
2019-10-03 15:23:08 -0700

    add companyView component and get info printing on screen

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import masterCompanyList from './companies';
-
+import CompanyView from './CompanyView'
 
 export default function Company(props) {
 
     console.log("COMPANY PROPS:", props)
-    
+    //below, onclick has to be anon 
     return (
-    <div >
+    <div>
         <h1>Company</h1>
-        <p onClick={() => props.viewCompany(props.company.companyName)} >{props.company.companyName}</p>
-        <p>{props.company.coreServices}</p>
+        <p onClick={() => props.viewCompany(props.company.companyName)} >{props.company.companyName}</p> 
+        <CompanyView company={props.company}/>
+        {/* <p>{props.company.coreServices}</p> */}
     </div>
     )
 };
